7915b6ef7d8ebaae0f3a70720c9576e43a28aa0e
Aarav Goyal <gl.aarav@gmail.com>
2025-04-02 14:52:27 -0700

    Add files via upload

---
+++ PutItTogether.java
@@ -0,0 +1,189 @@
+/*
+ * Aarav Goyal
+ * 3.31.2025
+ * PutItTogether.java
+ */ 
+
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Image;
+
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+import java.awt.GridLayout;
+import java.awt.CardLayout;
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;  
+import javax.swing.JPanel;
+
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.ButtonGroup;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import javax.swing.JSlider;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class PutItTogether
+{	
+	
+	public static void main(String [] args)
+	{
+		PutItTogether pit = new PutItTogether();
+		pit.run();
+	}
+	
+	public void run()
+	{
+		JFrame frame = new JFrame("PutItTogether");
+		frame.setSize( 800, 800);				
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE); 
+		frame.setLocation(00,50);
+		frame.setResizable(true);
+		PutItTogetherHolder pith = new PutItTogetherHolder(); 		
+		frame.getContentPane().add( pith );		
+		frame.setVisible(true);		
+	}
+}
+
+class PutItTogetherHolder extends JPanel 
+{	
+	public PutItTogetherHolder()
+	{
+		setBackground(Color.CYAN);
+
+		CardLayout cards = new CardLayout();
+		setLayout(cards);
+		
+		Information info = new Information();
+		FirstPagePanel fpp = new FirstPagePanel(this, cards, info);
+		FixedPanelHolder hph = new FixedPanelHolder(info);
+		
+		add(fpp, "First");
+		add(hph, "Home");
+	}
+}
+
+// First page to show up.  Gives information, asks for name.  Goes to Home page.
+class FirstPagePanel extends JPanel
+{
+	private PutItTogetherHolder panelCards;
+	private CardLayout cards;
+	private Information info;
+	private JTextField tfName;
+	
+	public FirstPagePanel(PutItTogetherHolder panelCardsIn, CardLayout cardsIn, Information infoIn)
+	{
+
+	}
+}
+
+class FixedPanelHolder extends JPanel
+{
+	private Information info;
+	private JButton homeButton;
+	
+	public FixedPanelHolder(Information infoIn)
+	{
+		
+	}
+}
+
+class HomePanelHolder extends JPanel
+{
+	private Image picture;
+	private Information info;
+	private String pictName;
+	private CardLayout cards;
+	
+	public HomePanelHolder (Information infoIn)
+	{
+		
+	}
+
+	public CardLayout getCardLayout()
+	{
+		return cards;
+	}
+}
+
+class HomePanel extends JPanel
+{
+	
+	// Since the label for the name was created when the classes constructor was called
+	// it needs to be updated after the user types in the name into the text field.
+	// Update that label in paintComponent.
+	public void paintComponent(Graphics g)
+	{
+		
+	}
+}
+
+class BothPictPanel extends JPanel implements MouseListener
+{
+	
+}
+
+class MyPictPanel extends JPanel implements ActionListener
+{
+	
+}
+
+
+class FriendPictPanel extends JPanel implements ActionListener
+{
+	
+}
+
+class DrawPanel extends JPanel
+{
+	private RightPanel rp;
+	private int amtRed, amtGreen, amtBlue;
+	private int size;
+	
+	public class LeftPanel extends JPanel
+	{
+		
+	}
+	
+	public class RightPanel extends JPanel
+	{
+		
+	}
+}
+
+class Information
+{
+	private String name;
+	
+	public Information()
+	{
+		
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String nameIn)
+	{
+		name = nameIn;
+	}
+}
+
